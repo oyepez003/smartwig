ecae01c4469726ff16be71a3f42678bb75a0da81
Omar Yepez <oyepez003@gmail.com>
2014-04-24 13:51:49 -0430

    [update] Last stable version. MIT License

--- lib/Yepsua/SmarTwig/Twig/TokenParser/ContentTokenParser.php
+++ lib/Yepsua/SmarTwig/Twig/TokenParser/ContentTokenParser.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SmarTwig (Twig Extension).
+ *
+ * (c) Omar Yepez <omar.yepez@yepsua.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Yepsua\SmarTwig\Twig\TokenParser;
 
 use Yepsua\SmarTwig\Twig\Node\ContentNode;
